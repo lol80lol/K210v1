a4570155c44fceee1672e3071c69a79584c48537
lol80lol <lallan12top45@gmail.com>
2026-03-31 11:55:07 +0530

    v1.8 with mic+ cam built on docker ubuntu 20.04 py version 3.8

--- components/micropython/port/builtin_py/_boot.py
+++ components/micropython/port/builtin_py/_boot.py
@@ -42,57 +42,51 @@ del ide, ide_mode_conf
 main_py = '''
 try:
     import gc, lcd, image, sys, os
-    from Maix import GPIO
-    from fpioa_manager import fm
-    test_pin=16
-    fm.fpioa.set_function(test_pin,fm.fpioa.GPIO7)
-    test_gpio=GPIO(GPIO.GPIO7,GPIO.IN,GPIO.PULL_UP)
-    fm.fpioa.set_function(17,fm.fpioa.GPIO0)
-    lcd_en = GPIO(GPIO.GPIO0,GPIO.OUT)
-    lcd_en.value(0)
+    gc.collect()
     lcd.init()
-    lcd.clear(color=(255,0,0))
-    lcd.draw_string(lcd.width()//2-68,lcd.height()//2-4, "Welcome to ", lcd.WHITE, lcd.RED)
-    if test_gpio.value() == 0:
-        print('PIN 16 pulled down, enter test mode')
-        lcd.clear(lcd.PINK)
-        lcd.draw_string(lcd.width()//2-68,lcd.height()//2-4, "Test Mode, wait ...", lcd.WHITE, lcd.PINK)
-        import sensor
-        import image
-        sensor.reset()
-        sensor.set_pixformat(sensor.RGB565)
-        sensor.set_framesize(sensor.QVGA)
-        sensor.run(1)
-        lcd.freq(16000000)
-        while True:
-            img=sensor.snapshot()
-            lcd.display(img)
 
-    loading = image.Image(size=(lcd.width(), lcd.height()))
-    loading.draw_rectangle((0, 0, lcd.width(), lcd.height()), fill=True, color=(255, 0, 0))
-    info = "Welcome to MaixPy-v1"
-    loading.draw_string(int((lcd.width() - len(info) * 10)//2), (lcd.height())//4, info, color=(255, 255, 255), scale=2, mono_space=0)
+    w = lcd.width()
+    h = lcd.height()
+
+    # Create canvas and fill White
+    img = image.Image(size=(w, h))
+    img.draw_rectangle((0, 0, w, h), fill=True, color=(255, 255, 255))
+
+    # Welcome line (Black)
+    info = "Welcome to VisionCore"
+    img.draw_string(int(w//2 - len(info) * 5), h//4, info, color=(0, 0, 0), scale=2)
+
+    # STEMbotix in two colors, centered
+    text1 = "STEM"
+    text2 = "botix"
+    scale = 2
+    char_w = 8 * scale # MaixPy built-in font approx width
+    total_w = (len(text1) + len(text2)) * char_w
+    x0 = int(w//2 - total_w//2)
+    y0 = h//3 + 20
+    img.draw_string(x0, y0, text1, color=(236, 100, 43), scale=scale, mono_space=1)
+    img.draw_string(x0 + len(text1)*char_w, y0, text2, color=(44, 40, 114), scale=scale, mono_space=1)
+
+    # Version info (Black) - Fixed positioning
     v = sys.implementation.version
-    vers = 'V{}.{}.{}'.format(v[0],v[1],v[2])
-    loading.draw_string(int((lcd.width() - len(vers) * 8)//2), (lcd.height())//3 + 20, vers, color=(255, 255, 255), scale=1, mono_space=1)
-    vers = 'MaixPy v4 is published'.format(v[0],v[1],v[2])
-    loading.draw_string(int((lcd.width() - len(vers) * 8)//2), (lcd.height())//3 + 40, vers, color=(255, 255, 255), scale=1, mono_space=1)
-    vers = 'Visit wiki.sipeed.com/maixpy'.format(v[0],v[1],v[2])
-    loading.draw_string(int((lcd.width() - len(vers) * 8)//2), (lcd.height())//3 + 60, vers, color=(255, 255, 255), scale=1, mono_space=1)
-    vers = 'And  wiki.sipeed.com/maixcam'.format(v[0],v[1],v[2])
-    loading.draw_string(int((lcd.width() - len(vers) * 8)//2), (lcd.height())//3 + 80, vers, color=(255, 255, 255), scale=1, mono_space=1)
-    lcd.display(loading)
-    tf = None
+    vers = "v{}.{}.{}".format(v[0], v[1], v[2])
+    img.draw_string(10, h - 20, vers, color=(0, 0, 0), scale=1)
+
+    # SD Card Detection Logic
     try:
-            os.listdir("/sd/.")
-    except Exception as e:
-        tf ="SDcard not found, use flash!"
-        loading.draw_string(int((lcd.width() - len(tf) * 8)//2), lcd.height() - 32, tf, color=(255, 255, 255), scale=1, mono_space=1)
-    if not tf:
-        tf ="SDcard found, mount at /sd/"
-        loading.draw_string(int((lcd.width() - len(tf) * 8)//2), lcd.height() - 32, tf, color=(255, 255, 255), scale=1, mono_space=1)
-    lcd.display(loading)
-    del loading, v, info, vers
+        os.listdir("/sd")
+        tf = "SDcard found, mount at /sd/"
+        status_color = (0, 128, 0) # Green for success
+    except:
+        tf = "SDcard not found, use flash!"
+        status_color = (255, 0, 0) # Red for warning
+
+    # Draw SD status in Black or Status Color (NOT White)
+    img.draw_string(int((w - len(tf) * 7)//2), h - 40, tf, color=(0, 0, 0), scale=1)
+
+    # Final single display call
+    lcd.display(img)
+    del img, v, info, vers
     gc.collect()
 finally:
     gc.collect()
@@ -145,17 +139,4 @@ try:
 except Exception:
     pass
 
-banner = '''
- __  __              _____  __   __  _____   __     __
-|  \/  |     /\     |_   _| \ \ / / |  __ \  \ \   / /
-| \  / |    /  \      | |    \ V /  | |__) |  \ \_/ /
-| |\/| |   / /\ \     | |     > <   |  ___/    \   /
-| |  | |  / ____ \   _| |_   / . \  | |         | |
-|_|  |_| /_/    \_\ |_____| /_/ \_\ |_|         |_|
-
-Official Site : https://www.sipeed.com
-Wiki          : https://wiki.sipeed.com/maixpy
-'''
-print(banner)
-del banner
 
