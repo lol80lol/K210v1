0e06ac215c6b9c0676007bcf905df0bc1ff302e3
lol80lol <lallan12top45@gmail.com>
2026-04-08 10:33:17 +0530

    Added fs folder for SPIFFS

--- components/micropython/port/builtin_py/_boot.py
+++ components/micropython/port/builtin_py/_boot.py
@@ -41,55 +41,7 @@ del ide, ide_mode_conf
 # detect boot.py
 main_py = '''
 try:
-    import gc, lcd, image, sys, os
-    gc.collect()
-    lcd.init()
-
-    w = lcd.width()
-    h = lcd.height()
-
-    # Create canvas and fill White
-    img = image.Image(size=(w, h))
-    img.draw_rectangle((0, 0, w, h), fill=True, color=(255, 255, 255))
-
-    # Welcome line (Black)
-    info = "Welcome to VisionCore"
-    img.draw_string(int(w//2 - len(info) * 5), h//4, info, color=(0, 0, 0), scale=2)
-
-    # STEMbotix in two colors, centered
-    text1 = "STEM"
-    text2 = "botix"
-    scale = 2
-    char_w = 8 * scale # MaixPy built-in font approx width
-    total_w = (len(text1) + len(text2)) * char_w
-    x0 = int(w//2 - total_w//2)
-    y0 = h//3 + 20
-    img.draw_string(x0, y0, text1, color=(236, 100, 43), scale=scale, mono_space=1)
-    img.draw_string(x0 + len(text1)*char_w, y0, text2, color=(44, 40, 114), scale=scale, mono_space=1)
-
-    # Version info (Black) - Fixed positioning
-    v = sys.implementation.version
-    vers = "v{}.{}.{}".format(v[0], v[1], v[2])
-    img.draw_string(10, h - 20, vers, color=(0, 0, 0), scale=1)
-
-    # SD Card Detection Logic
-    try:
-        os.listdir("/sd")
-        tf = "SDcard found, mount at /sd/"
-        status_color = (0, 128, 0) # Green for success
-    except:
-        tf = "SDcard not found, use flash!"
-        status_color = (255, 0, 0) # Red for warning
-
-    # Draw SD status in Black or Status Color (NOT White)
-    img.draw_string(int((w - len(tf) * 7)//2), h - 40, tf, color=(0, 0, 0), scale=1)
-
-    # Final single display call
-    lcd.display(img)
-    del img, v, info, vers
-    gc.collect()
-finally:
-    gc.collect()
+    import main_app
 '''
 
 flash_ls = os.listdir()
